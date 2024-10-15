4c7f204d31a66c35469308af5c7a55dc6151b36c
mohammadsajjadjamali <mohammadsajjadjamali@gmail.com>
2024-10-15 19:37:41 +0330

    upload project on github

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ['./*.html'],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: { 
+    fontFamily: {
+      vazir : ['Vazir']
+    },
+    extend: { 
+      lineHeight: {
+        '0': '0',
+       }
+    },
+  },
+  plugins: []
+}
